60a27633956d06f3bc95524943e4ae64cf18c25f
joeuni <dmsdl8917@gmail.com>
2023-11-23 20:28:37 +0900

    이모지 수정

--- src/components Navbar/Navbar/Navbar.jsx
+++ src/components Navbar/Navbar/Navbar.jsx
@@ -1,9 +1,7 @@
 import { NavLink } from "react-router-dom";
-import Fire from "../../assets/fire.png";
-import Star from "../../assets/glowing-star.png";
-import Party from "../../assets/partying-face.png";
+
 import DarkMode from "../DarkMode/DarkMode";
-import Search from "../../assets/search.png";
+
 import "./Navbar.css";
 
 export default function Navbar() {
@@ -12,22 +10,10 @@ export default function Navbar() {
       <h1>MovieApp</h1>
       <div className="navbar_links">
         <DarkMode />
-        <NavLink to="/">
-          인기작품
-          <img className="navbar_emoji" src={Fire} alt="fire emoji" />
-        </NavLink>
-        <NavLink to="/top_rated">
-          최고평점
-          <img className="navbar_emoji" src={Star} alt="star emoji" />
-        </NavLink>
-        <NavLink to="/upcoming">
-          상영예정
-          <img className="navbar_emoji" src={Party} alt="party emoji" />
-        </NavLink>
-        <NavLink to="/search">
-          영화검색
-          <img className="navbar_emoji" src={Search} alt="search emoji" />
-        </NavLink>
+        <NavLink to="/">인기작품</NavLink>
+        <NavLink to="/top_rated">최고평점</NavLink>
+        <NavLink to="/upcoming">상영예정</NavLink>
+        <NavLink to="/search">영화검색 </NavLink>
       </div>
     </nav>
   );
